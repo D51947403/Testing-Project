511eb392eff7658bb8103b3c2e790ef621f9a2e2
dkumar-clgx <dkumarsingraul@corelogic.com>
2022-10-19 17:11:04 +0530

    Future Demo

---
+++ java-core-demo-app/src/singraul/multithreading/FutureDemo.java
@@ -0,0 +1,14 @@
+package singraul.multithreading;
+/**
+ * 
+ * @author 2106725
+ *
+ */
+public class FutureDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
